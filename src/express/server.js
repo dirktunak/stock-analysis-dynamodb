f5f50a61681a891939d07194c6faf4c75cdee4bc
Derek Tanaka <tanaka.derek@gmail.com>
2019-12-04 16:27:21 -0500

    accept authorization headers

--- src/express/server.js
+++ src/express/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept')
     next()
 })
 
